3ea306e7f992c19183611b433fd7d7e23486c5d6
Sylvain Pichon <loisirs@cussou.fr>
2025-12-19 00:00:02 +0100

    chore: bump version to 1.8.1 for Play Store release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.cussou.autotiq"
         minSdk = 26  // Aligned with Fairtiq app (Android 8.0 Oreo)
         targetSdk = 36  // Aligned with Fairtiq app
-        versionCode = 5
-        versionName = "1.8.0"
+        versionCode = 6
+        versionName = "1.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
